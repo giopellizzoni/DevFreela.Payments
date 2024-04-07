e355d51d2f0e98b69fc7d8e4b972d9817053361d
Giovanni Pellizzoni <pelissoni@gmail.com>
2024-04-07 11:12:10 +0200

    Implementing Microservice for payment

---
+++ DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -0,0 +1,24 @@
+﻿using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Payments.API.Controllers
+{
+    [Route("api/payments")]
+    public class PaymentsController(IPaymentService paymentService) : ControllerBase
+    {
+        
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PaymentInfoInputModel paymentInfoInputModel)
+        {
+            var result = await paymentService.Process(paymentInfoInputModel);
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Payments.API.Models;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService: IPaymentService
+    {
+        
+
+        public Task<bool> Process(PaymentInfoInputModel inputModel)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
